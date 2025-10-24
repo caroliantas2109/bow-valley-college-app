dc48a53e08e361798bba419f6b37e7560f949102
GhettoBaby <buokemchinwendu@gmail.com>
2025-10-24 15:26:56 -0600

    subtle home page motion

--- src/pages/home/home-page.jsx
+++ src/pages/home/home-page.jsx
@@ -4,10 +4,11 @@ function HomePage() {
     return <div className={styles.homePage}>
         <div className={`${styles.homePageCover} flex-center-all`}>
             <div className={styles.heroText}>
-                <p className='text-heading fs-big-2 fw-xxl'>Course <span className='orange-gradient-text bg-clip-text'>registration</span></p>
-                <p className='capitalize-text fs-mid-1 fw-m'>for software devs, by software devs</p>
-                <button className='btn1 text-body fw-m'>Sign in</button>
+                <p className={`text-heading fs-big-2 fw-xxl ${styles.slideInLeft}`}>Course <span className='orange-gradient-text bg-clip-text'>registration</span></p>
+                <p className={`capitalize-text fs-mid-1 fw-m ${styles.slideInLeft}`}>for software devs, by software devs</p>
+                <button className={`btn1 text-body fw-m ${styles.slideUp}`}>Sign in</button>
             </div>
+            {/*there is going to be animation here */}
             <p className={styles.paragraphText}>Hit the<br /> ground <br /> working.</p>
         </div>
     </div>
