5f784c237b884ae3a66fb9e938f32273450c8d55
Carol Iantas <iantascarol@outlook.com>
2025-10-27 17:51:46 -0600

    Refactor App component to use AdminDashboard

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { students, admins, courses, programs } from './data/mockData';
-import './styles/common.css';
+// import './styles/Common.css';
+// import './styles/HomePage.css';
+// import './styles/Dashboard.css';
+import AdminDashboard from './pages/admin/AdminDashboard';
 
 function App() {
   // Test if data imports correctly
@@ -11,19 +14,10 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Data Import Test</h1>
-      <p>Check the browser console (F12) to see if data loaded correctly</p>
-      <div style={{ padding: '20px' }}>
-        <h2>Test Students:</h2>
-        <p>First Student: {students[0].firstName} {students[0].lastName}</p>
-        <p>Student ID: {students[0].studentId}</p>
-        
-        <h2>Test Courses:</h2>
-        <p>First Course: {courses[0].code} - {courses[0].name}</p>
-      </div>
+
+      <AdminDashboard></AdminDashboard>
     </div>
+
+
   );
 }
-
-export default App;
-
